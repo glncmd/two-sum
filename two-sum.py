09b6efc9e06cb89acfd0c0283a935a435cad93b7
glncmd <77870057+glncmd@users.noreply.github.com>
2021-03-08 12:51:12 +0800

    Add files via upload

---
+++ two-sum.py
@@ -0,0 +1,14 @@
+def twoSum(nums, target):
+    i = 0
+    for num in nums:
+        i2 = i+1
+        for num2 in nums[i2:]:
+            if num + num2 == target:
+                return [i, i2]
+            else:
+                i2 += 1
+                continue
+        else:
+            i += 1
+    else:
+        return 'N/A'
